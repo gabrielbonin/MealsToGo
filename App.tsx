f153d9724a62a23013a24dc33b4e94d15de74db1
Gabriel Bonin <gabrielbonin@MacBook-Pro-de-Gabriel.local>
2023-01-04 20:25:46 -0300

    feat: safeAreaView

--- App.tsx
+++ App.tsx
@@ -3,9 +3,24 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          padding: 20,
+          backgroundColor: "green",
+        }}
+      >
+        <Text>Search</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View
+        style={{
+          padding: 20,
+          backgroundColor: "blue",
+          flex: 1,
+        }}
+      >
+        <Text>List</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
