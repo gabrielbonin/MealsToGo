e17edea1af5410953172f9059d5f4a4d9be8ac61
Gabriel Bonin <gabriel.bonin@mblabs.com.br>
2023-01-15 16:06:37 -0300

    improve eslint and prettier setup and search bar from react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,32 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import React, { useState } from "react";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={{
-          padding: 20,
-          backgroundColor: "green",
-        }}
-      >
-        <Text>Search</Text>
-        <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+        />
       </View>
-      <View
-        style={{
-          padding: 20,
-          backgroundColor: "blue",
-          flex: 1,
-        }}
-      >
+      <View style={styles.list}>
         <Text>List</Text>
-        <StatusBar style="auto" />
+        <ExpoStatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 20,
+    backgroundColor: "green",
+  },
+  list: {
+    padding: 20,
+    backgroundColor: "blue",
+    flex: 1,
+  },
 });
