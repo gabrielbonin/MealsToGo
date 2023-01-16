570b2ea773684e74c82f05afb9e9fc3b76e50fab
Gabriel Bonin <gabriel.bonin@mblabs.com.br>
2023-01-15 21:16:37 -0300

    improve Styled-components migration

---
+++ src/feature/restaurants/components/restaurant-info-card.component.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+import { Text } from "react-native";
+
+export const Title = styled(Text)`
+  padding: 12px;
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+export const RestaurantCard = styled(Card)``;
+
+export const RestaurantCardCover = styled(Card.Cover)``;
