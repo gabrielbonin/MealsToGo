28a167e9ee5de4c24eb8867db83d7da8c8dcbf39
Gabriel Bonin <gabriel.bonin@mblabs.com.br>
2023-02-23 21:17:50 -0300

    improve reestaurant-info-card

--- src/feature/restaurants/components/restaurant-info-card.component.styles.ts
+++ src/feature/restaurants/components/restaurant-info-card.component.styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 import { Text, View } from "react-native";
+import { SvgXml } from "react-native-svg";
 
 import { space } from "../../../infrastructure/theme/spacing";
 import { fonts, fontSizes } from "../../../infrastructure/theme/fonts";
@@ -19,6 +20,30 @@ export const RestaurantCard = styled(Card)``;
 
 export const RestaurantCardCover = styled(Card.Cover)``;
 
+export const StarIcon = styled(SvgXml)`
+  margin-right: ${space[2]};
+`;
+
+export const OpenIcon = styled(SvgXml)`
+  flex-direction: row;
+`;
+
 export const Info = styled(View)`
   padding: ${space[3]};
+`;
+
+export const Rating = styled(View)`
+  flex-direction: row;
+  padding-vertical: ${space[2]};
+`;
+
+export const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
--- src/feature/restaurants/components/restaurant-info-card.component.tsx
+++ src/feature/restaurants/components/restaurant-info-card.component.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import * as S from "./restaurant-info-card.component.styles";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Text } from "react-native";
 interface RestaurantInfoProps {
   restaurant: {
     name: string;
@@ -9,6 +12,7 @@ interface RestaurantInfoProps {
     openingHours: boolean;
     rating: number;
     isClosedTemporarily: boolean;
+    isOpenNow: boolean;
   };
 }
 
@@ -19,14 +23,29 @@ export const RestaurantInfo = ({ restaurant }: RestaurantInfoProps) => {
     photos,
     address,
     openingHours,
-    rating,
-    isClosedTemporarily,
+    rating = 3,
+    isClosedTemporarily = true,
+    isOpenNow = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <S.RestaurantCard elevation={5}>
       <S.RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <S.Info>
         <S.Title>{name}</S.Title>
+        <S.Section>
+          <S.Rating>
+            {ratingArray.map((_, i) => (
+              <S.StarIcon key={i} xml={star} width={20} height={20} />
+            ))}
+          </S.Rating>
+          <S.SectionEnd>
+            {isClosedTemporarily && <Text>Temporarily Closed</Text>}
+            {isOpenNow && <S.OpenIcon xml={open} width={20} height={20} />}
+          </S.SectionEnd>
+        </S.Section>
         <S.Address>{address}</S.Address>
       </S.Info>
     </S.RestaurantCard>
